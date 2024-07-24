40a9f0c8a2209e58042972b94e693729ae909462
AdamSeungheonShin <ssh9809@naver.com>
2024-07-24 15:05:43 +0900

    📦 chore: prettier, lint 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,3 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      Main
-    </main>
-  );
+  return <main className="flex size-2"></main>;
 }
