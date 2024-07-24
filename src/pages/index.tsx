6cb31efccdcf2e9dab7cc094d351e9868a23a64e
AdamSeungheonShin <ssh9809@naver.com>
2024-07-24 18:13:50 +0900

    📦 chore: 초기세팅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import Image from 'next/image';
+import React from 'react';
+
+import next from '../../public/next.svg';
+
 export default function Home() {
-  return <main className="flex size-2"></main>;
+  return (
+    <main className="flex h-2 w-3 text-ellipsis text-stone-300">
+      <Image src={next} width={20} height={20} alt="text" />
+    </main>
+  );
 }
