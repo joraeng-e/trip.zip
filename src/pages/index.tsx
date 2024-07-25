0c4d44a69d8f25936e994443c29510a7bca8aff6
doyo <83871696+doh-yo@users.noreply.github.com>
2024-07-25 14:48:27 +0900

    ✨ feat: 아이콘 등록 (#8)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
-import Image from 'next/image';
 import React from 'react';
-
-import next from '../../public/next.svg';
 
 export default function Home() {
   return (
     <main className="flex h-2 w-3 text-ellipsis text-stone-300">
-      <Image src={next} width={20} height={20} alt="text" />
+      메인페이지
     </main>
   );
 }
