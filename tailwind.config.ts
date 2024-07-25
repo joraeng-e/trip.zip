503582a3a5449c1ba796ae13c8bdd343f60fcfac
dudwns0213 <dudwns0213@gmail.com>
2024-07-25 22:01:47 +0900

    💄 JE-27-ui: tailwind px로 테마 커스터마이징

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from 'tailwindcss';
 
+const px0_10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) };
+const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
+const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
+const px0_2000 = { ...Array.from(Array(2001)).map((_, i) => `${i}px`) };
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,6 +13,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      borderWidth: px0_10,
+      fontSize: px0_100,
+      lineHeight: px0_100,
+      minWidth: px0_200,
+      minHeight: px0_200,
+      spacing: px0_200,
+      maxWidth: px0_2000,
+      maxHeight: px0_2000,
       colors: {
         'custom-gray': {
           100: '#FAFAFA',
