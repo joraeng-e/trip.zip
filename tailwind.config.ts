8c37ab578178e13b220a1695ce285e89dfcc2f76
YoungJun Bae <dudwns0213@gmail.com>
2024-07-25 22:07:26 +0900

    📦 JE-27-chore: tailwind setting (#12)
    * 💄 JE-27-ui: global font pretendard 설정
    
    * 📦 JE-27-chore: tailwind config 확장(color, fontsize)
    
    * 💄 JE-27-ui: 기본 container css 지정
    
    * ✅ JE-27-test: style test
    
    * 💄ul: JE-27-tailwind layer name 수정:tailwind layer name 수정
    
    * 💄 JE-27-ui: tailwind px로 테마 커스터마이징
    
    * 🐛 JE-27-fix: keyvalue build error 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,18 @@
 import type { Config } from 'tailwindcss';
 
+const px0_10 = Object.fromEntries(
+  Array.from(Array(11)).map((_, i) => [`${i}`, `${i}px`]),
+);
+const px0_100 = Object.fromEntries(
+  Array.from(Array(101)).map((_, i) => [`${i}`, `${i}px`]),
+);
+const px0_200 = Object.fromEntries(
+  Array.from(Array(201)).map((_, i) => [`${i}`, `${i}px`]),
+);
+const px0_2000 = Object.fromEntries(
+  Array.from(Array(2001)).map((_, i) => [`${i}`, `${i}px`]),
+);
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,7 +20,84 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: px0_10,
+      fontSize: px0_100,
+      lineHeight: px0_100,
+      minWidth: px0_200,
+      minHeight: px0_200,
+      spacing: px0_200,
+      maxWidth: px0_2000,
+      maxHeight: px0_2000,
+      colors: {
+        'custom-gray': {
+          100: '#FAFAFA',
+          200: '#EEEEEE',
+          300: '#DDDDDD',
+          400: '#CBC9CF',
+          500: '#ADAEB8',
+          600: '#A4A1AA',
+          700: '#79747E',
+          800: '#4B4B4B',
+        },
+        'nomad-black': '#112211',
+        'custom-black': '#1B1B1B',
+        'custom-green': {
+          100: '#CED8D5',
+          200: '#0B3B2D',
+          300: '#00AC07',
+        },
+        'custom-red': {
+          100: '#FFE4E0',
+          200: '#FF472E',
+        },
+        'custom-orange': {
+          100: '#FFF4E8',
+          200: '#FF7C1D',
+        },
+        'custom-yellow': '#FFC23D',
+        'custom-blue': {
+          100: '#E5F3FF',
+          200: '#2EB4FF',
+          300: '#0085FF',
+        },
+      },
+    },
+    fontSize: {
+      '3xl-bold': ['32px', { lineHeight: '46px', fontWeight: 'bold' }],
+
+      '2xl-bold': ['24px', { lineHeight: '32px', fontWeight: 'bold' }],
+      '2xl-semibold': ['24px', { lineHeight: '32px', fontWeight: '600' }],
+      '2xl-medium': ['24px', { lineHeight: '32px', fontWeight: '500' }],
+      '2xl-regular': ['24px', { lineHeight: '32px', fontWeight: '400' }],
+
+      'xl-bold': ['20px', { lineHeight: '32px', fontWeight: 'bold' }],
+      'xl-semibold': ['20px', { lineHeight: '32px', fontWeight: '600' }],
+      'xl-medium': ['20px', { lineHeight: '32px', fontWeight: '500' }],
+      'xl-regular': ['20px', { lineHeight: '32px', fontWeight: '400' }],
+
+      '2lg-bold': ['18px', { lineHeight: '26px', fontWeight: 'bold' }],
+      '2lg-semibold': ['18px', { lineHeight: '26px', fontWeight: '600' }],
+      '2lg-medium': ['18px', { lineHeight: '26px', fontWeight: '500' }],
+      '2lg-regular': ['18px', { lineHeight: '26px', fontWeight: '400' }],
+
+      'lg-bold': ['16px', { lineHeight: '26px', fontWeight: 'bold' }],
+      'lg-semibold': ['16px', { lineHeight: '26px', fontWeight: '600' }],
+      'lg-medium': ['16px', { lineHeight: '26px', fontWeight: '500' }],
+      'lg-regular': ['16px', { lineHeight: '26px', fontWeight: '400' }],
+
+      'md-bold': ['14px', { lineHeight: '24px', fontWeight: 'bold' }],
+      'md-semibold': ['14px', { lineHeight: '24px', fontWeight: '600' }],
+      'md-medium': ['14px', { lineHeight: '24px', fontWeight: '500' }],
+      'md-regular': ['14px', { lineHeight: '24px', fontWeight: '400' }],
+
+      'sm-semibold': ['13px', { lineHeight: '22px', fontWeight: '600' }],
+      'sm-medium': ['13px', { lineHeight: '22px', fontWeight: '500' }],
+
+      'xs-semibold': ['12px', { lineHeight: '20px', fontWeight: '600' }],
+      'xs-medium': ['12px', { lineHeight: '18px', fontWeight: '500' }],
+      'xs-regular': ['12px', { lineHeight: '18px', fontWeight: '400' }],
+    },
   },
   plugins: [],
 };
