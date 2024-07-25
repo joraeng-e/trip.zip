c068d8da3c630ee752a6ed59e83966c148f6044f
doh-yo <doyo159090@gmail.com>
2024-07-25 12:35:42 +0900

    ✨ feat: 아이콘 등록

---
+++ src/libs/utils/Icon.tsx
@@ -0,0 +1,43 @@
+import xIcon from '/public/icon/XIcon.svg';
+import bedIcon from '/public/icon/bedIcon.svg';
+import calendarIcon from '/public/icon/calendarIcon.svg';
+import deleteIcon from '/public/icon/deleteIcon.svg';
+import facebookIcon from '/public/icon/facebookIcon.svg';
+import instagramIcon from '/public/icon/instagramIcon.svg';
+import kebabIcon from '/public/icon/kebabIcon.svg';
+import minusTimeIcon from '/public/icon/minusTimeIcon.svg';
+import notificationIcon from '/public/icon/notificationIcon.svg';
+import passwordOffIcon from '/public/icon/passwordOffIcon.svg';
+import passwordOnIcon from '/public/icon/passwordOnIcon.svg';
+import plusTimeIcon from '/public/icon/plusTimeIcon.svg';
+import profileAccountIcon from '/public/icon/profileAccountIcon.svg';
+import profileCalendarIcon from '/public/icon/profileCalendarIcon.svg';
+import profileChecklistICon from '/public/icon/profileChecklistICon.svg';
+import profileCogIcon from '/public/icon/profileCogIcon.svg';
+import starOffIcon from '/public/icon/starOffIcon.svg';
+import starOnIcon from '/public/icon/starOnIcon.svg';
+import twitterIcon from '/public/icon/twitterIcon.svg';
+import youtubeIcon from '/public/icon/youtubeIcon.svg';
+
+export {
+  bedIcon,
+  calendarIcon,
+  deleteIcon,
+  youtubeIcon,
+  facebookIcon,
+  instagramIcon,
+  twitterIcon,
+  kebabIcon,
+  minusTimeIcon,
+  plusTimeIcon,
+  notificationIcon,
+  passwordOffIcon,
+  passwordOnIcon,
+  profileAccountIcon,
+  profileCalendarIcon,
+  profileChecklistICon,
+  profileCogIcon,
+  starOffIcon,
+  starOnIcon,
+  xIcon,
+};
