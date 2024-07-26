8a8076f4c59c6d5d7d9fdc406fb26c33aa0b5974
김보미 <kimbomi2172@naver.com>
2024-07-26 21:01:39 +0900

    📝 JE-10-docs: passwordInput jsdoc 추가

--- src/components/input/PasswordInput.tsx
+++ src/components/input/PasswordInput.tsx
@@ -9,6 +9,19 @@ type PasswordInputProps = {
   error?: FieldError;
 };
 
+/**
+ * 비밀번호 입력 필드 컴포넌트로, 비밀번호 표시/숨기기 기능을 포함합니다.
+ *
+ * @param {string} name - 입력 필드의 이름
+ * @param {string} placeholder - 입력 필드의 플레이스홀더 텍스트
+ * @param {UseFormRegisterReturn} register - react-hook-form의 register 함수 반환값
+ * @param {FieldError} [error] - 입력 필드의 에러 정보 (선택적)
+ * @returns {JSX.Element} - 렌더링된 PasswordInput 컴포넌트
+ *
+ *
+ * @author 김보미
+ */
+
 export default function PasswordInput({
   name,
   placeholder,
