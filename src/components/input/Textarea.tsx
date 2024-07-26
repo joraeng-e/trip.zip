254a63067600ea1b0f33893a62db3f1d0f30e652
김보미 <kimbomi2172@naver.com>
2024-07-26 20:42:32 +0900

    ♻️ JE-10-refactor: textarea props 수정

--- src/components/input/Textarea.tsx
+++ src/components/input/Textarea.tsx
@@ -1,70 +1,66 @@
-import React from 'react';
+import React, { TextareaHTMLAttributes } from 'react';
 import { FieldError, UseFormRegisterReturn } from 'react-hook-form';
 
-/**
- * @example
- * const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>({
-    resolver: yupResolver(schema),
-  });
-
-  const onSubmit: SubmitHandler<FormData> = (data) => {
-    console.log('폼 제출', data);
-  };
-  
- * <Textarea
-    label="내용"
-    name="content"
-    placeholder="내용을 입력해주세요"
-    register={register('price')}
-    error={errors.password}
-    />
- * TextareaProps 타입 정의
- * @typedef {Object} TextareaProps
- * @property {string} label - 텍스트영역의 레이블
- * @property {string} name - 텍스트영역의 이름
- * @property {string} placeholder - 텍스트영역의 플레이스홀더 텍스트
- * @property {UseFormRegisterReturn} register - react-hook-form의 register 함수 반환값
- * @property {FieldError} [error] - 텍스트영역의 에러 정보 (선택적)
- * @property {string} [maxWidth='640px'] - (반응형을 위한)입력 필드 컨테이너의 최대 너비. 기본값 640px
- * @property {string} [height='346px'] - (반응형을 위한)입력 필드의 높이. 기본값 346px
- * @author 김보미
- */
-
-type TextareaProps = {
+type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement> & {
   label: string;
-  name: string;
-  placeholder: string;
   register: UseFormRegisterReturn;
   error?: FieldError;
   maxWidth?: string;
   height?: string;
 };
 
+/**
+ * Textarea 컴포넌트
+ * @param {string} label - 텍스트영역의 레이블
+ * @param {UseFormRegisterReturn} register - react-hook-form의 register 함수 반환값
+ * @param {FieldError} [error] - 텍스트영역의 에러 정보 (선택적)
+ * @param {string} [maxWidth='640px'] - (반응형을 위한)입력 필드 컨테이너의 최대 너비. 기본값 640px
+ * @param {string} [height='346px'] - (반응형을 위한)입력 필드의 높이. 기본값 346px
+ * @returns {JSX.Element} - 렌더링된 Textarea 컴포넌트
+ * @example
+ * const {
+ *   register,
+ *   handleSubmit,
+ *   formState: { errors },
+ * } = useForm<FormData>({
+ *   resolver: yupResolver(schema),
+ * });
+ *
+ * const onSubmit: SubmitHandler<FormData> = (data) => {
+ *   console.log('폼 제출', data);
+ * };
+ *
+ * <Textarea
+ *   label="내용"
+ *   name="content"
+ *   placeholder="내용을 입력해주세요"
+ *   register={register('content')}
+ *   error={errors.content}
+ * />
+ *
+ * @author 김보미
+ */
+
 export default function Textarea({
   label,
-  name,
-  placeholder,
   register,
   error,
   maxWidth = '640px',
   height = '346px',
+  ...props
 }: TextareaProps) {
+  const { name, placeholder } = props;
+
   return (
-    <div
-      className={`relative flex w-full flex-col gap-2`}
-      style={{ maxWidth: maxWidth }}
-    >
+    <div className="relative flex w-full flex-col gap-2" style={{ maxWidth }}>
       <label htmlFor={name}>{label}</label>
       <textarea
         id={name}
         placeholder={placeholder}
         {...register}
-        className={`resize-none rounded-md border-2 p-16 outline-none focus:border-custom-green-200 ${error && 'border-red-400'}`}
-        style={{ height: height }}
+        className={`resize-none rounded-md border-2 p-16 outline-none focus:border-custom-green-200 ${error ? 'border-red-400' : ''}`}
+        style={{ height }}
+        {...props}
       />
       {error && (
         <p className="pl-8 text-xs-regular text-custom-red-200">
