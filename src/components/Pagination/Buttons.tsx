13f9ff163d9762588e23a0ec54d7cbd55b9b8f12
alexgoni <goni000211@gmail.com>
2024-07-26 11:52:49 +0900

    ✨ JE-13-feat: Pagination UI

--- src/components/Pagination/Buttons.tsx
+++ src/components/Pagination/Buttons.tsx
@@ -1,13 +1,89 @@
+import classNames from '@/libs/utils/classNames';
+import Image from 'next/image';
+import { ReactNode } from 'react';
+
+import { usePaginationContext } from './Root';
+
+// TODO: svg 컴포넌트로 변경(disabled 스타일 추가)
 export function PaginationPrevButton() {
-  return <></>;
+  const { size } = usePaginationContext();
+  const disabled = true;
+
+  const classnames = classNames(
+    'flex items-center justify-center border border-custom-green-200 rounded-[15px] bg-white hover:bg-gray-100',
+    {
+      'size-55': size === 'md',
+      'size-40': size === 'sm',
+      'border-custom-green-200': !disabled,
+      'border-custom-gray-300': disabled,
+    },
+  );
+
+  return (
+    <button type="button" className={classnames}>
+      <Image
+        src={'/icon/paginationArrowLeft.svg'}
+        width={21}
+        height={21}
+        alt="prev"
+      />
+    </button>
+  );
 }
+
 export function PaginationNextButton() {
-  return <></>;
+  const { size } = usePaginationContext();
+  const disabled = true;
+
+  const classnames = classNames(
+    'flex items-center justify-center border rounded-[15px] bg-white hover:bg-gray-100',
+    {
+      'size-55': size === 'md',
+      'size-40': size === 'sm',
+      'border-custom-green-200': !disabled,
+      'border-custom-gray-300': disabled,
+    },
+  );
+
+  return (
+    <button type="button" className={classnames}>
+      <Image
+        src={'/icon/paginationArrowRight.svg'}
+        width={21}
+        height={21}
+        alt="prev"
+      />
+    </button>
+  );
 }
-function PaginationNumber() {
-  return <></>;
+
+function PaginationNumber({ children }: { children: ReactNode }) {
+  const { size } = usePaginationContext();
+  const active = true;
+
+  const classnames = classNames(
+    'flex items-center justify-center rounded-[15px]',
+    {
+      'size-55': size === 'md',
+      'size-40': size === 'sm',
+      'bg-custom-green-200 text-white hover:bg-[#0b553e]': active,
+      'bg-white hover:bg-gray-100 border border-custom-green-200': !active,
+    },
+  );
+
+  return (
+    <button type="button" className={classnames}>
+      {children}
+    </button>
+  );
 }
 
 export function PaginationNumbers() {
-  return <></>;
+  return (
+    <>
+      {Array.from({ length: 5 }, (_, index) => (
+        <PaginationNumber key={index}>{index + 1}</PaginationNumber>
+      ))}
+    </>
+  );
 }
--- src/components/Pagination/Root.tsx
+++ src/components/Pagination/Root.tsx
@@ -1,5 +1,36 @@
-import { ReactNode } from 'react';
+import { ReactNode, createContext, useContext } from 'react';
 
-export default function PaginationRoot({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+interface PaginationContextType {
+  size: 'sm' | 'md';
+}
+
+const PaginationContext = createContext<PaginationContextType>({
+  size: 'md',
+});
+
+export const usePaginationContext = () => {
+  const context = useContext(PaginationContext);
+
+  if (!context) {
+    throw new Error('Pagination 컨텍스트를 호출할 수 없는 범위입니다.');
+  }
+
+  return context;
+};
+
+interface Props {
+  children: ReactNode;
+  size?: 'sm' | 'md';
+}
+
+export default function PaginationRoot({ children, size = 'md' }: Props) {
+  const contextValue = {
+    size,
+  };
+
+  return (
+    <PaginationContext.Provider value={contextValue}>
+      <div className="flex gap-10">{children}</div>
+    </PaginationContext.Provider>
+  );
 }
