6df501617812d76e1c98c6d99d756f6435e048c0
김보미 <kimbomi2172@naver.com>
2024-07-27 16:51:13 +0900

    💄 JE-14-ui: 회원가입 폼 UI 구현

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,7 +1,9 @@
+import Button from '@/components/button';
 import Input from '@/components/input/Input';
-import Textarea from '@/components/input/Textarea';
 import { schema } from '@/libs/utils/schema';
 import { yupResolver } from '@hookform/resolvers/yup';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
@@ -25,52 +27,66 @@ export default function Signup() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Input
-        label="이메일"
-        name="email"
-        type="text"
-        placeholder="이메일을 입력해주세요"
-        register={register('email')}
-        error={errors.email}
-        maxWidth="500px"
-      />
-      <Input
-        label="비밀번호"
-        name="password"
-        type="password"
-        placeholder="비밀번호를 입력해주세요"
-        register={register('password')}
-        error={errors.password}
-      />
-      <Input
-        label="가격"
-        name="price"
-        type="number"
-        placeholder="가격을 입력해주세요"
-        register={register('price')}
-        error={errors.password}
-      />
-      <Textarea
-        label="내용"
-        name="content"
-        placeholder="내용을 입력해주세요"
-        register={register('price')}
-        error={errors.password}
-      />
-      {/* <Input
-        label="닉네임"
-        name="nickname"
-        type="text"
-        placeholder="닉네임을 입력해주세요"
-      />
-      <Input
-        label="비밀번호 확인"
-        name="password"
-        type="password"
-        placeholder="비밀번호를 한번 더 입력해주세요"
-      /> */}
-      <button type="submit">제출</button>
-    </form>
+    <div className="page-container">
+      <div className="flex flex-col items-center justify-center">
+        <Link href="/" aria-label="메인페이지로 이동">
+          <Image
+            src="/logo/tripzip.png"
+            alt="trip.zip"
+            width={300}
+            height={20}
+          />
+        </Link>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex w-full max-w-640 flex-col gap-16"
+        >
+          <Input
+            label="이메일"
+            name="email"
+            type="text"
+            placeholder="이메일을 입력해 주세요"
+            register={register('email')}
+            error={errors.email}
+          />
+          <Input
+            label="닉네임"
+            name="nickname"
+            type="text"
+            placeholder="닉네임을 입력해 주세요"
+            register={register('password')}
+            error={errors.password}
+          />
+          <Input
+            label="비밀번호"
+            name="password"
+            type="password"
+            placeholder="8자 이상 입력해 주세요"
+            register={register('password')}
+            error={errors.password}
+          />
+          <Input
+            label="비밀번호 확인"
+            name="password"
+            type="password"
+            placeholder="비밀번호를 한번 더 입력해 주세요"
+            register={register('password')}
+            error={errors.password}
+          />
+          <Button type="submit" className="rounded-md" variant="disabledButton">
+            회원가입 하기
+          </Button>
+        </form>
+        <div className="text-md mt-20 flex gap-8">
+          <p>회원이신가요?</p>
+          <Link
+            href="login"
+            className="text-custom-green-200 underline decoration-custom-green-200 underline-offset-2"
+          >
+            로그인하기
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
