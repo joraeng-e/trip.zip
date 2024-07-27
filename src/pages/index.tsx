785e3f1a2e46881e0bbad63493ae28c74118bc23
doh-yo <doyo159090@gmail.com>
2024-07-27 14:38:12 +0900

    📦 JE-17-chore: index 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
-import Footer from '@/components/footer';
 import React from 'react';
 
 export default function Home() {
   return (
-    <main className="bg-gray-500">
+    <main className="basic-container bg-gray-500">
       <button className="flex-center h-35 rounded-full border-2 px-10 text-24">
         버튼
       </button>
       <div className="text-xl-bold text-custom-orange-200"></div>
       메인페이지
-      <Footer />
     </main>
   );
 }
