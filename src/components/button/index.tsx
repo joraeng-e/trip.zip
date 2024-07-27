d5837ae2e50df950274d0f31904da8c13fb601ef
doh-yo <doyo159090@gmail.com>
2024-07-27 09:41:48 +0900

    📦 JE-9-chore: 피드백 수정

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,18 +2,6 @@ import { PaperPlaneIcon } from '@/libs/utils/Icon';
 import { motion } from 'framer-motion';
 import { DetailedHTMLProps, PropsWithChildren } from 'react';
 
-/**
- * 버튼 공용 컴포넌트입니다.
- *
- * @param {Object} props - 컴포넌트의 속성들.
- * @param {'activeButton' | 'inactiveButton' | 'disabledButton'} [props.variant='activeButton'] - 버튼의 상태를 지정합니다.
- * @param {boolean} [props.icon=false] - 비행기 아이콘을 포함할지 여부를 지정합니다.
- * @param {string} [props.className=''] - 사용자 측에서 추가적인 스타일을 지정합니다.
- * @param {React.ReactNode} props.children - 버튼의 자식 요소로, 보통 버튼 텍스트가 포함됩니다.
- * @param {Object} rest - 기타 버튼 속성들로, `React.ButtonHTMLAttributes<HTMLButtonElement>`가 허용하는 모든 속성을 포함합니다.
- *
- */
-
 interface ButtonProps
   extends PropsWithChildren<
     DetailedHTMLProps<
@@ -22,14 +10,26 @@ interface ButtonProps
     >
   > {
   variant?: 'activeButton' | 'inactiveButton' | 'disabledButton';
-  icon?: boolean;
+  hasICon?: boolean;
   className?: string;
 }
+
+/**
+ * 버튼 공용 컴포넌트입니다.
+ *
+ * @param {Object} props - 컴포넌트의 속성들.
+ * @param {'activeButton' | 'inactiveButton' | 'disabledButton'} [props.variant='activeButton'] - 버튼의 상태를 지정합니다.
+ * @param {boolean} [props.hasICon=false] - 비행기 아이콘을 포함할지 여부를 지정합니다.
+ * @param {string} [props.className=''] - 사용자 측에서 추가적인 스타일을 지정합니다.
+ * @param {React.ReactNode} props.children - 버튼의 자식 요소로, 보통 버튼 텍스트가 포함됩니다.
+ * @param {Object} rest - 기타 버튼 속성들로, `React.ButtonHTMLAttributes<HTMLButtonElement>`가 허용하는 모든 속성을 포함합니다.
+ *
+ */
 
 export default function Button({
   children,
   variant = 'activeButton',
-  icon = false,
+  hasICon = false,
   className = '',
   ...rest
 }: ButtonProps) {
@@ -70,7 +70,7 @@ export default function Button({
       <button className={variant} {...rest}>
         {children}
       </button>
-      {icon && <PaperPlaneIcon width={25} height={25} className="ml-6" />}
+      {hasICon && <PaperPlaneIcon width={25} height={25} className="ml-6" />}
     </motion.div>
   );
 }
