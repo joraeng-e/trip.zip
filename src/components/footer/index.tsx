0c6e9c09bb64c14137d10b3d2593142751b6b826
doyo <83871696+doh-yo@users.noreply.github.com>
2024-07-27 16:22:33 +0900

    ✨ JE-17-feat: Footer 추가 (#20)
    * ✨ JE-17-feat: Footer 추가
    
    * 💄 JE-17-style: Footer 스타일 수정
    
    * 📦 JE-17-chore: index 수정
    
    * 📦 JE-17-chore: Link 추가

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,48 @@
+import {
+  FacebookIcon,
+  InstagramIcon,
+  TwitterIcon,
+  YoutubeIcon,
+} from '@/libs/utils/Icon';
+import Link from 'next/link';
+
+export default function Footer() {
+  return (
+    <div className="w-full bg-nomad-black px-38 pb-66 pt-32 md:gap-60 md:px-200 md:pb-108">
+      <div className="flex-center flex-col gap-24 md:flex-row md:justify-between">
+        <div className="flex-center w-full gap-12 md:basis-1/2 md:justify-between lg:basis-1/2 [&_>div]:text-[#676767]">
+          <div>
+            <Link href="https://github.com/joraeng-e/trip.zip">
+              <p>@Joraeng-e</p>
+            </Link>
+          </div>
+          <div className="flex gap-12 md:gap-30">
+            <Link href="/privacy-policy">
+              <p className="whitespace-nowrap">Privacy Policy</p>
+            </Link>
+            <Link href="/faq">
+              <p>FAQ</p>
+            </Link>
+          </div>
+        </div>
+        <div className="flex-center gap-12">
+          <Link href="https://www.facebook.com">
+            <FacebookIcon aria-label="페이스북" />
+          </Link>
+          <Link href="https://x.com">
+            <TwitterIcon
+              aria-label="트위터"
+              className="h-20 w-20 rounded-md bg-white"
+            />
+          </Link>
+          <Link href="https://www.youtube.com">
+            <YoutubeIcon aria-label="유튜브" />
+          </Link>
+          <Link href="https://www.instagram.com">
+            <InstagramIcon aria-label="인스타그램" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
