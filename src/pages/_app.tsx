cb9faf5ea57c69181e4005cfb9764b264f06951e
AdamSeungheonShin <ssh9809@naver.com>
2024-07-27 15:09:58 +0900

    ✨ JE-30-feat: update jsdoc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,10 +15,26 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   /**
    * 레이아웃이 적용된 페이지 컴포넌트 반환해주는 함수
+   *
    * 페이지 컴포넌트 index에서 해당 함수 호출 시에만 레이아웃이 적용
    *
    * @param {ReactElement} page - 레이아웃으로 감싸줄 컴포넌트
    * @returns {ReactNode} 레이아웃이 적용된 컴포넌트 반환
+   *
+   * ```tsx
+   * import Layout from '@/components/pageLayout/pageLayout';
+   * import type { ReactElement } from 'react';
+   *
+   * export default PageName = () => {
+   *   return <div>About Page</div>;
+   * };
+   *
+   * PageName.getLayout = (page: ReactElement) => (
+   *   <Layout showHeader={false} showFooter={true}>
+   *     {page}
+   *   </Layout>
+   * );
+   * ```
    */
   const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
 
