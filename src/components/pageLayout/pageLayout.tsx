ba03f4195d97ed615a63a37a5255293bc4e8b000
AdamSeungheonShin <ssh9809@naver.com>
2024-07-27 15:00:55 +0900

    ✨ JE-30-feat: add getLayout func, add jsdoc

--- src/components/pageLayout/pageLayout.tsx
+++ src/components/pageLayout/pageLayout.tsx
@@ -12,10 +12,19 @@ type LayoutProps = {
 const PRIVATE_PATHS = ['/private'];
 // Todo : "내 정보, 예약내역, 내 체험 관리, 내 체험 등록, 내 체험 수정, 예약 현황, 알림(모달), 후기 작성(모달)" PRIVATE_PATH list에 추가
 
+/**
+ * 페이지 컴포넌트를 감싸는 레이아웃 컴포넌트입니다🙇🏻‍♂️
+ * Private path로 접근하는 경우 로그인여부 확인
+ *
+ * @param {ReactNode} children - 페이지 컴포넌트
+ * @param {boolean} [showHeader=true] - header 렌더링 여부(optional, default true)
+ * @param {boolean} [showFooter=true] - footer 렌더링 여부(optional, default true)
+ * @returns {JSX.Element} 렌더링 여부에 따라 header, footer 적용된 layout 반환
+ */
 export default function Layout({
   children,
-  showHeader,
-  showFooter,
+  showHeader = true,
+  showFooter = true,
 }: LayoutProps) {
   const pathname = usePathname();
   const router = useRouter();
@@ -26,4 +35,5 @@ export default function Layout({
   }
 
   return <>{children}</>;
+  // Todo : header, footer 완성되는 대로 추가하기
 }
