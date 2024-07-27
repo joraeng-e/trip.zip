f3da1cabd95a00cad81ec475b2b384382ad29404
dudwns0213 <dudwns0213@gmail.com>
2024-07-28 04:30:34 +0900

    ✨ JE-12-feat: modal content xicon 조건부 렌더링 기능 구현

--- src/components/Modal/Content.tsx
+++ src/components/Modal/Content.tsx
@@ -1,11 +1,15 @@
 import { XIcon } from '@/libs/utils/Icon';
-import { PropsWithChildren, useRef } from 'react';
+// XIcon import
+import { FC, PropsWithChildren, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 import { useModalContext } from './Root';
 
-export default function ModalContent(props: PropsWithChildren) {
-  const { children } = props;
+interface ModalContentProps extends PropsWithChildren {
+  Icon?: boolean; // Icon prop 추가 (boolean 타입)
+}
+
+const ModalContent: FC<ModalContentProps> = ({ children, Icon }) => {
   const { open: currentOpenState, handleOpenChange } = useModalContext();
   const overlayRef = useRef<HTMLDivElement>(null);
 
@@ -18,6 +22,12 @@ export default function ModalContent(props: PropsWithChildren) {
     }
   };
 
+  const handleClose = () => {
+    if (handleOpenChange) {
+      handleOpenChange(false);
+    }
+  };
+
   const renderPortal = () => {
     if (currentOpenState) {
       return createPortal(
@@ -26,7 +36,13 @@ export default function ModalContent(props: PropsWithChildren) {
           className="fixed inset-0 -z-50 flex items-center justify-center bg-gray-500/30"
           onClick={handleClickOverlay}
         >
-          <div className="rounded-10 first-line:bg-grayscale-50 z-50 mx-2 flex h-auto w-[335px] flex-col bg-white p-[20px] shadow-lg md:w-[395px]">
+          <div className="rounded-10 first-line:bg-grayscale-50 relative z-50 mx-2 flex h-auto w-[335px] flex-col bg-white p-[20px] shadow-lg md:w-[395px]">
+            {/* Icon prop이 true일 때만 XIcon을 렌더링 */}
+            {Icon && (
+              <button onClick={handleClose} className="absolute right-4 top-4">
+                <XIcon />
+              </button>
+            )}
             {children}
           </div>
         </div>,
@@ -37,4 +53,6 @@ export default function ModalContent(props: PropsWithChildren) {
   };
 
   return <>{renderPortal()}</>;
-}
+};
+
+export default ModalContent;
