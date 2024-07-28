5cad583f715710fa73944623f740b2bbe22af39d
Adam Shin <137388618+AdamSeungheonShin@users.noreply.github.com>
2024-07-28 17:13:41 +0900

    ✨ JE-30-feat: layout (#21)
    * ✨ JE-30-feat: layout component
    
    * ✨ JE-30-feat: add getLayout func, add jsdoc
    
    * ✨ JE-30-feat: update jsdoc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,42 @@
+import Layout from '@/components/pageLayout/pageLayout';
 import '@/styles/globals.css';
+import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import { ReactElement, ReactNode } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  /**
+   * 레이아웃이 적용된 페이지 컴포넌트 반환해주는 함수
+   *
+   * 페이지 컴포넌트 index에서 해당 함수 호출 시에만 레이아웃이 적용
+   *
+   * @param {ReactElement} page - 레이아웃으로 감싸줄 컴포넌트
+   * @returns {ReactNode} 레이아웃이 적용된 컴포넌트 반환
+   *
+   * ```tsx
+   * import Layout from '@/components/pageLayout/pageLayout';
+   * import type { ReactElement } from 'react';
+   *
+   * export default PageName = () => {
+   *   return <div>About Page</div>;
+   * };
+   *
+   * PageName.getLayout = (page: ReactElement) => (
+   *   <Layout showHeader={false} showFooter={true}>
+   *     {page}
+   *   </Layout>
+   * );
+   * ```
+   */
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
+
+  return getLayout(<Component {...pageProps} />);
 }
