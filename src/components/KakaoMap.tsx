a89c953efa70d2eec55943dd19a8d5410d9db850
dudwns0213 <dudwns0213@gmail.com>
2024-07-30 03:05:54 +0900

    ✨ JE-43-feat: daum 주소 api를 변환하여 kakaomap 좌표로 지정, 위치 이동하는 기능 구현

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -1,20 +1,69 @@
-import Script from 'next/script';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { kakaoscript } from '@/libs/constants/kakaoscript';
+import React, { useEffect, useRef } from 'react';
+import 'react-kakao-maps-sdk';
 
-const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=b89ff2565658129d4c11ec14915247de&autoload=false`;
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
-export default function KakaoMap() {
+interface KakaoMapProps {
+  address: string;
+}
+
+export default function KakaoMap({ address }: KakaoMapProps) {
+  const mapRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = kakaoscript;
+    script.async = true;
+
+    script.onload = () => {
+      const { kakao } = window;
+
+      kakao.maps.load(() => {
+        const options = {
+          center: new kakao.maps.LatLng(33.450701, 126.570667),
+          level: 3,
+        };
+
+        const map = new kakao.maps.Map(mapRef.current, options);
+        const geocoder = new kakao.maps.services.Geocoder();
+
+        geocoder.addressSearch(address, (result: any, status: any) => {
+          if (status === window.kakao.maps.services.Status.OK) {
+            const latitude: number = Number(result[0].y);
+            const longitude: number = Number(result[0].x);
+            const coords = new kakao.maps.LatLng(latitude, longitude);
+
+            const marker = new kakao.maps.Marker({
+              map: map,
+              position: coords,
+            });
+
+            marker.setMap(map); // 마커 렌더링
+            map.panTo(coords);
+          } else {
+            console.error('주소 검색 실패:', status);
+          }
+        });
+      });
+    };
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, [address]);
+
   return (
     <>
-      <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
-      <Map
-        center={{ lat: 33.5563, lng: 126.79581 }}
-        style={{ width: '100%', height: '360px' }}
-      >
-        <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-          <div style={{ color: '#000' }}>Hello World!</div>
-        </MapMarker>
-      </Map>
+      <div className="flex items-center justify-center pt-2">
+        <div ref={mapRef} className="h-500 w-[95%]" />
+      </div>
     </>
   );
 }
