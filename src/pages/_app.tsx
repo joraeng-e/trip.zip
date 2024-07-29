c59999ad1563e5544ecd70bca05621dad2680c1e
김보미 <kimbomi2172@naver.com>
2024-07-29 11:50:29 +0900

    ♻️ JE-14-refactor: app layout 재설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,49 @@
 import Layout from '@/components/pageLayout/pageLayout';
 import '@/styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import type { ReactElement, ReactNode } from 'react';
 
 const queryClient = new QueryClient();
 
-export default function App({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  /**
+   * 레이아웃이 적용된 페이지 컴포넌트 반환해주는 함수
+   *
+   * 페이지 컴포넌트 index에서 해당 함수 호출 시에만 레이아웃이 적용
+   *
+   * @param {ReactElement} page - 레이아웃으로 감싸줄 컴포넌트
+   * @returns {ReactNode} 레이아웃이 적용된 컴포넌트 반환
+   *
+   * ```tsx
+   * import Layout from '@/components/pageLayout/pageLayout';
+   * import type { ReactElement } from 'react';
+   *
+   * export default PageName = () => {
+   *   return <div>About Page</div>;
+   * };
+   *
+   * PageName.getLayout = (page: ReactElement) => (
+   *   <Layout showHeader={false} showFooter={true}>
+   *     {page}
+   *   </Layout>
+   * );
+   * ```
+   */
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </QueryClientProvider>
   );
 }
