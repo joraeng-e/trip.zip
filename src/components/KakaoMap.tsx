e18800faac144f874cfe3965dc20e8dc5784e81b
YoungJun Bae <dudwns0213@gmail.com>
2024-07-30 16:47:33 +0900

    ✨ JE-43-feat: address, script 생성 로직 분리 및 address 없을 시 에러 수정

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { kakaoscript } from '@/libs/constants/kakaoscript';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'react-kakao-maps-sdk';
 
 declare global {
@@ -43,49 +43,64 @@ interface KakaoMapProps {
 
 export default function KakaoMap({ address }: KakaoMapProps) {
   const mapRef = useRef<HTMLDivElement>(null);
+  const [scriptFlag, setScriptFlag] = useState(false);
 
   useEffect(() => {
     const script = document.createElement('script');
     script.src = kakaoscript;
     script.async = true;
-
     script.onload = () => {
-      const { kakao } = window;
-
-      kakao.maps.load(() => {
-        const options = {
-          center: new kakao.maps.LatLng(33.450701, 126.570667),
-          level: 3,
-        };
-
-        const map = new kakao.maps.Map(mapRef.current, options);
-        const geocoder = new kakao.maps.services.Geocoder();
-
-        geocoder.addressSearch(address, (result: any, status: any) => {
-          if (status === window.kakao.maps.services.Status.OK) {
-            const latitude: number = Number(result[0].y);
-            const longitude: number = Number(result[0].x);
-            const coords = new kakao.maps.LatLng(latitude, longitude);
-
-            const marker = new kakao.maps.Marker({
-              map: map,
-              position: coords,
-            });
-
-            marker.setMap(map); // 마커 렌더링
-            map.panTo(coords);
-          } else {
-            console.error('주소 검색 실패:', status);
-          }
-        });
-      });
+      setScriptFlag(true);
     };
+
     document.head.appendChild(script);
 
     return () => {
       document.head.removeChild(script);
     };
-  }, [address]);
+  }, []);
+
+  useEffect(() => {
+    if (!scriptFlag) return;
+
+    const { kakao } = window;
+
+    kakao.maps.load(() => {
+      const options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+
+      const map = new kakao.maps.Map(mapRef.current, options);
+      const geocoder = new kakao.maps.services.Geocoder();
+
+      if (!address) {
+        const marker = new kakao.maps.Marker({
+          map,
+        });
+        marker.setMap(map);
+        return;
+      }
+
+      geocoder.addressSearch(address, (result: any, status: any) => {
+        if (status === window.kakao.maps.services.Status.OK) {
+          const latitude: number = Number(result[0].y);
+          const longitude: number = Number(result[0].x);
+          const coords = new kakao.maps.LatLng(latitude, longitude);
+
+          const marker = new kakao.maps.Marker({
+            map: map,
+            position: coords,
+          });
+
+          marker.setMap(map); // 마커 렌더링
+          map.panTo(coords);
+        } else {
+          console.error('주소 검색 실패:', status);
+        }
+      });
+    });
+  }, [address, scriptFlag]);
 
   return (
     <>
