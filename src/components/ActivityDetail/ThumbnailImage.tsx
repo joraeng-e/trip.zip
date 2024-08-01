277feee4773a77b1fbc6fabbf449ac0e7e68d194
YoungJun Bae <dudwns0213@gmail.com>
2024-08-01 22:53:49 +0900

    ✨ JE-36-feat: ThumbnailImage 컴포넌트 구현

---
+++ src/components/ActivityDetail/ThumbnailImage.tsx
@@ -0,0 +1,22 @@
+// ThumbnailImage.tsx
+import Image from 'next/image';
+
+interface ThumbnailImageProps {
+  src: string;
+  alt: string;
+  onClick: () => void;
+}
+
+export default function ThumbnailImage(props: ThumbnailImageProps) {
+  const { src, alt, onClick } = props;
+  return (
+    <div className="relative h-60 w-60 cursor-pointer" onClick={onClick}>
+      <Image
+        src={src}
+        alt={alt}
+        layout="fill"
+        className="rounded-lg object-cover"
+      />
+    </div>
+  );
+}
