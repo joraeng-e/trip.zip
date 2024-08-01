0d2bca1329934d7cce461ded4030dbb6c74f598d
YoungJun Bae <dudwns0213@gmail.com>
2024-08-01 21:54:36 +0900

    💄 JE-36-ui: Title ui

--- src/components/ActivityDetail/Title.tsx
+++ src/components/ActivityDetail/Title.tsx
@@ -1,3 +1,5 @@
+import { KebabIcon } from '@/libs/utils/Icon';
+import { LocationIcon } from '@/libs/utils/Icon';
 import React from 'react';
 
 interface TitleProps {
@@ -12,13 +14,16 @@ export default function Title(props: TitleProps) {
   const { title, address, category, rating, reviewCount } = props;
 
   return (
-    <div>
-      <div className="">{category}</div>
-      <div>{title}</div>
-      <div>
-        {rating}({reviewCount})
+    <div className="mx-16 mt-16 flex-col">
+      <div className="text-md-regular text-nomad-black">{category}</div>
+      <div className="mb-16 mt-10 flex items-center justify-between text-2xl-bold text-nomad-black">
+        {title} <KebabIcon className="" />
       </div>
-      <div>{address}</div>
+      <div className="flex items-center gap-12 text-md-regular text-custom-black">
+        ⭐{rating}({reviewCount})
+        <LocationIcon />
+        <div>{address}</div>
+      </div>
     </div>
   );
 }
