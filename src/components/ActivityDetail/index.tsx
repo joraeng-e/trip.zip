15f31c6317036c455b6661668c255f1283a60408
YoungJun Bae <dudwns0213@gmail.com>
2024-08-01 22:08:54 +0900

    ✨ JE-36-feat: MobileBannerImage export

--- src/components/ActivityDetail/index.tsx
+++ src/components/ActivityDetail/index.tsx
@@ -1,5 +1,6 @@
 export { default as Title } from './Title';
 export { default as BannerImage } from './BannerImage';
+export { default as MobileBannerImage } from './MobileBannerImage';
 export { default as Description } from './Description';
 export { default as Address } from './Address';
 export { default as Review } from './Review';
