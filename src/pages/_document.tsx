0fdd52d9576fb75a9a7ce703a09c542998811483
doh-yo <doyo159090@gmail.com>
2024-08-02 15:30:11 +0900

    📦 JE-34-chore: 테스트 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,6 @@ export default function Document() {
     <Html lang="ko">
       <Head />
       <link rel="icon" href="/logo/tripzipFavicon.png" type="image/png" />
-      <script
-        src="https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
-        async
-      ></script>
       <body>
         <Main />
         <NextScript />
