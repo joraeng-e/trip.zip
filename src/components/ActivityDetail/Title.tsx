ea639120837dadd6b05e62314ba0eb562b16512c
dudwns0213 <dudwns0213@gmail.com>
2024-08-05 01:16:49 +0900

    ✨ JE-36-feat: title 주소 복사 기능 추가

--- src/components/ActivityDetail/Title.tsx
+++ src/components/ActivityDetail/Title.tsx
@@ -13,16 +13,27 @@ interface TitleProps {
 export default function Title(props: TitleProps) {
   const { title, address, category, rating, reviewCount } = props;
 
+  const handleClickClipboard = () => {
+    navigator.clipboard.writeText(address).then(() => {
+      alert('주소가 클립보드에 복사되었습니다!');
+    });
+  };
+
   return (
     <div className="mx-16 mt-16 flex-col">
       <div className="text-md-regular text-nomad-black">{category}</div>
       <div className="mb-16 mt-10 flex items-center justify-between text-2xl-bold text-nomad-black">
         {title} <KebabIcon className="" />
       </div>
-      <div className="flex items-center gap-12 text-md-regular text-custom-black">
+      <div className="flex gap-12 text-md-regular text-custom-black">
         ⭐{rating}({reviewCount})
-        <LocationIcon />
-        <div>{address}</div>
+        <div
+          className="flex cursor-pointer items-center gap-12"
+          onClick={handleClickClipboard}
+        >
+          <LocationIcon />
+          <div>{address}</div>
+        </div>
       </div>
     </div>
   );
