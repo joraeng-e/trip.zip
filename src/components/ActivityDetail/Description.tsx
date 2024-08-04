1aa756ef989fd3a3d181278f611397cc427aa926
dudwns0213 <dudwns0213@gmail.com>
2024-08-05 03:03:31 +0900

    🚚 JE-36-chore: 함수 이름 수정

--- src/components/ActivityDetail/Description.tsx
+++ src/components/ActivityDetail/Description.tsx
@@ -10,33 +10,36 @@ export default function Description(props: DescriptionProps) {
 
   const [isExpanded, setIsExpanded] = useState(false); // 내용이 확장되었는지 여부
 
-  const displayedDescription = isExpanded
+  const handleTextDisplay = isExpanded
     ? description
     : `${description.slice(0, maxLength)}...`;
 
   return (
-    <div className="m-16">
-      <h2 className="mb-16 mt-20 text-xl-bold text-nomad-black">체험 설명</h2>
-      <div className="text-lg-regular text-nomad-black">
-        {displayedDescription}
+    <>
+      <div className="contour"></div>
+      <div className="m-16">
+        <h2 className="mb-16 mt-20 text-xl-bold text-nomad-black">체험 설명</h2>
+        <div className="text-lg-regular text-nomad-black">
+          {handleTextDisplay}
+        </div>
+        {/* 더 보기 버튼 */}
+        {description.length > maxLength && !isExpanded && (
+          <button
+            className="mt-2 text-custom-gray-700"
+            onClick={() => setIsExpanded(true)}
+          >
+            더 보기
+          </button>
+        )}
+        {isExpanded && (
+          <button
+            className="mt-2 text-custom-gray-700"
+            onClick={() => setIsExpanded(false)}
+          >
+            접기
+          </button>
+        )}
       </div>
-      {/* 더 보기 버튼 */}
-      {description.length > maxLength && !isExpanded && (
-        <button
-          className="mt-2 text-custom-gray-700"
-          onClick={() => setIsExpanded(true)}
-        >
-          더 보기
-        </button>
-      )}
-      {isExpanded && (
-        <button
-          className="mt-2 text-custom-gray-700"
-          onClick={() => setIsExpanded(false)}
-        >
-          접기
-        </button>
-      )}
-    </div>
+    </>
   );
 }
