fd84d35d09aff0578e1b742a6bffeb7df52c20a7
dudwns0213 <dudwns0213@gmail.com>
2024-08-05 03:49:22 +0900

    💄 JE-36-ui: activitytags ui

--- src/components/commons/ReviewTag/index.tsx
+++ src/components/commons/ReviewTag/index.tsx
@@ -11,7 +11,8 @@ import {
   SpongeEmoji,
   StarEmoji,
 } from '@/libs/utils/Icon';
-import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import React from 'react';
 
 const tags = [
   { name: '스포츠', emoji: <SoccerBallEmoji /> },
@@ -33,19 +34,44 @@ interface ActivityTagsProps {
 
 const ActivityTags: React.FC<ActivityTagsProps> = ({ extractedTags }) => {
   return (
-    <div className="p-4">
-      <div className="mb-4 flex flex-wrap gap-2">
+    <div className="my-16">
+      <div className="flex flex-wrap gap-10">
         {tags
           .filter((tag) => extractedTags.includes(tag.name)) // 선택된 태그만 표시
           .map(({ name, emoji }) => (
-            <span
+            <motion.div
               key={name}
-              className="rounded-full bg-gray-200 px-3 py-1 text-gray-700"
+              className="flex items-center rounded-lg p-8"
+              style={{
+                background: 'linear-gradient(90deg, #6bfdfd, #22ebd0)',
+                backgroundSize: '400% 400%',
+                animation: 'gradient 15s ease infinite',
+              }}
+              whileHover={{
+                scale: 1.05,
+                transition: { duration: 0.3 },
+              }}
             >
-              {emoji} {name}
-            </span>
+              {emoji}
+              <span className="mx-4 text-lg-medium text-custom-gray-100">
+                {name}
+              </span>
+            </motion.div>
           ))}
       </div>
+      <style jsx>{`
+        @keyframes gradient {
+          0% {
+            background-position: 0% 50%;
+          }
+          50% {
+            background-position: 100% 50%;
+          }
+          100% {
+            background-position: 0% 50%;
+          }
+        }
+      `}</style>
     </div>
   );
 };
