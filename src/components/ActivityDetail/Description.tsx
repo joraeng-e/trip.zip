4d6ee3c500467948346b33567a94c359eb416fdb
dudwns0213 <dudwns0213@gmail.com>
2024-08-05 00:59:15 +0900

    💄 JE-36-ui: Description ui

--- src/components/ActivityDetail/Description.tsx
+++ src/components/ActivityDetail/Description.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import { useState } from 'react';
 
 interface DescriptionProps {
   description: string;
+  maxLength?: number;
 }
 
-export default function Description({ description }: DescriptionProps) {
+export default function Description(props: DescriptionProps) {
+  const { description, maxLength = 100 } = props;
+
+  const [isExpanded, setIsExpanded] = useState(false); // 내용이 확장되었는지 여부
+
+  const displayedDescription = isExpanded
+    ? description
+    : `${description.slice(0, maxLength)}...`;
+
   return (
     <div className="m-16">
-      <h2 className="text-xl-bold text-nomad-black">체험 설명</h2>
-      <div className="text-lg-regular text-nomad-black">{description}</div>
+      <h2 className="mb-16 mt-20 text-xl-bold text-nomad-black">체험 설명</h2>
+      <div className="text-lg-regular text-nomad-black">
+        {displayedDescription}
+      </div>
+      {/* 더 보기 버튼 */}
+      {description.length > maxLength && !isExpanded && (
+        <button
+          className="mt-2 text-custom-gray-700"
+          onClick={() => setIsExpanded(true)}
+        >
+          더 보기
+        </button>
+      )}
+      {isExpanded && (
+        <button
+          className="mt-2 text-custom-gray-700"
+          onClick={() => setIsExpanded(false)}
+        >
+          접기
+        </button>
+      )}
     </div>
   );
 }
