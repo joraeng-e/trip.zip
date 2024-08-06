f67262ce465aa93619496163ff22ee4013ce9731
dudwns0213 <dudwns0213@gmail.com>
2024-08-07 01:52:03 +0900

    ✨ JE-36-feat: sidebar 생성 및 header로도 변경되게 구현

--- src/pages/activity/[activityid].tsx
+++ src/pages/activity/[activityid].tsx
@@ -8,10 +8,9 @@ import {
   Review,
   Title,
 } from '@/components/ActivityDetail';
-import { GetActivityDetailResponse } from '@trip.zip-api';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function ActivityDetail() {
   const router = useRouter();
@@ -21,9 +20,38 @@ export default function ActivityDetail() {
     .map((image) => image.imageUrl)
     .filter((url) => url);
 
+  const [showButton, setShowButton] = useState(true);
+  const [showHeader, setShowHeader] = useState(false);
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+
+    if (scrollY > 400) {
+      // 400px 이상 스크롤 시 버튼 숨기고 헤더 표시
+      setShowButton(false);
+      setShowHeader(true);
+    } else {
+      setShowButton(true);
+      setShowHeader(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <div className="basic-container px-0">
+      {showHeader && (
+        <div className="sticky top-70 z-50 h-70 border-y-1 border-custom-gray-200 bg-white">
+          <h2>{DetailData.price} 원 / 인</h2>
+        </div>
+      )}
+      <div className="basic-container relative px-0">
         <Head>
           <title>{DetailData.title} - Trip.zip</title>
           <meta name="description" content={DetailData.description} />
@@ -32,35 +60,49 @@ export default function ActivityDetail() {
           <meta property="og:image" content={DetailData.bannerImageUrl} />
           <meta property="og:url" content={`${ActivityId}`} />
         </Head>
-        <div>data id: {DetailData.id}</div>
-        <div>router id: {ActivityId}</div>
         <div>
-          <Title
-            title={DetailData.title}
-            address={DetailData.address}
-            category={DetailData.category}
-            rating={DetailData.rating}
-            reviewCount={DetailData.reviewCount}
-          />
-          <div className="hidden md:block">
+          <div className="mt-10 hidden md:block">
             <BannerImage
               bannerImageUrl={DetailData.bannerImageUrl}
               subImageUrl={subImageUrls}
             />
           </div>
-          <div className="md:hidden">
+          <div className="mt-10 md:hidden">
             <MobileBannerImage
               bannerImageUrl={DetailData.bannerImageUrl}
               subImageUrl={subImageUrls}
             />
           </div>
-          <Description description={DetailData.description} />
-          <Address address={DetailData.address} />
-          <Review
-            averageRating={ReviewData.averageRating}
-            totalCount={ReviewData.totalCount}
-            reviews={ReviewData.reviews}
-          />
+
+          <div className="mt-10 flex">
+            <div className="mr-4 flex-1">
+              {/* 70% 차지 */}
+              <Title
+                title={DetailData.title}
+                address={DetailData.address}
+                category={DetailData.category}
+                rating={DetailData.rating}
+                reviewCount={DetailData.reviewCount}
+              />
+              <Description description={DetailData.description} />
+              <Address address={DetailData.address} />
+              <Review
+                averageRating={ReviewData.averageRating}
+                totalCount={ReviewData.totalCount}
+                reviews={ReviewData.reviews}
+              />
+            </div>
+            <div className="relative w-3/12">
+              {showButton && (
+                <div className="sticky top-100 h-300 w-full rounded-lg border-2 border-custom-gray-400">
+                  1000원 / 2인
+                  <button className="mt-2 w-full rounded-lg bg-blue-500 text-white">
+                    예약
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </>
