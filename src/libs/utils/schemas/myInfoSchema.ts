882ca7e7bc7a956d67c9b0f0e4352450d0275975
김보미 <kimbomi2172@naver.com>
2024-08-06 12:43:30 +0900

    ♻️ JE-60-refactor: 비밀번호 스키마 수정 (비어 있을 경우 유효성 검사 제외)

--- src/libs/utils/schemas/myInfoSchema.ts
+++ src/libs/utils/schemas/myInfoSchema.ts
@@ -8,13 +8,29 @@ export const myInfoSchema = yup.object().shape({
     .required('이메일을 입력해 주세요.'),
   newPassword: yup
     .string()
+    .test('min-length', '비밀번호는 최소 8자 이상이어야 합니다.', (value) => {
+      if (!value) return true; // 비어 있을 경우 유효성 검사 통과
+      return value.length >= 8;
+    })
+    .test(
+      'valid-password',
+      '비밀번호는 소문자, 숫자, 특수문자를 포함해야 하며 공백을 포함할 수 없습니다.',
+      (value) => {
+        if (!value) return true; // 비어 있을 경우 유효성 검사 통과
+
+        const hasLowercase = /[a-z]/.test(value);
+        const hasNumber = /[0-9]/.test(value);
+        const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+        const hasNoWhitespace = /^\S*$/.test(value);
+
+        return hasLowercase && hasNumber && hasSpecialChar && hasNoWhitespace;
+      },
+    )
     .nullable()
-    .matches(/[a-z]/, '소문자가 포함되어야 합니다.')
-    .matches(/[0-9]/, '숫자가 포함되어야 합니다.')
-    .matches(/[!@#$%^&*(),.?":{}|<>]/, '특수문자가 포함되어야 합니다.')
-    .matches(/^\S*$/, '공백을 포함할 수 없습니다.'),
+    .notRequired(),
   reEnterPassword: yup
     .string()
     .oneOf([yup.ref('newPassword')], '비밀번호가 일치하지 않습니다.')
-    .nullable(),
+    .nullable()
+    .notRequired(),
 });
