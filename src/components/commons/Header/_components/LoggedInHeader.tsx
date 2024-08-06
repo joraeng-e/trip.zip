4806ace1e1b3b606df25e677aa0a5082bb20f3ad
alexgoni <goni000211@gmail.com>
2024-08-06 23:09:52 +0900

    ✨ JE-67-feat: Get Data

--- src/components/commons/Header/_components/LoggedInHeader.tsx
+++ src/components/commons/Header/_components/LoggedInHeader.tsx
@@ -1,48 +1,14 @@
 import tripzipFavicon from '@/../public/logo/tripzipFavicon.png';
-import useClickOutside from '@/hooks/useClickOutside';
-import { useMediaQuery } from '@/hooks/useMediaQeury';
-import { NotificationIcon } from '@/libs/utils/Icon';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useRef, useState } from 'react';
+import React from 'react';
 
-import NotificationPopup, {
-  NotificationMobilePopup,
-} from './NotificationPopup';
+import Notification from './Notification';
 
 export default function LoggedInHeader() {
-  const [isNotificationPopupOpen, setIsNotificationPopupOpen] = useState(false);
-  const notificationRef = useRef<HTMLDivElement>(null);
-  const isMobile = useMediaQuery('(max-width: 767px)');
-
-  const toggleNotificationPopup = () => {
-    setIsNotificationPopupOpen((prev) => !prev);
-  };
-
-  const closePopup = () => {
-    setIsNotificationPopupOpen(false);
-  };
-
-  useClickOutside(notificationRef, closePopup);
-
   return (
     <div className="relative flex items-center gap-16">
-      <div
-        className="cursor-pointer"
-        aria-label="알림"
-        onClick={toggleNotificationPopup}
-      >
-        <NotificationIcon />
-      </div>
-      {isNotificationPopupOpen && (
-        <>
-          {!isMobile ? (
-            <NotificationPopup closePopup={closePopup} ref={notificationRef} />
-          ) : (
-            <NotificationMobilePopup closePopup={closePopup} />
-          )}
-        </>
-      )}
+      <Notification />
       <Link href="/mypage" className="flex items-center gap-6 border-l-2 pl-16">
         <div className="h-40 w-40 overflow-hidden rounded-full border">
           <Image
