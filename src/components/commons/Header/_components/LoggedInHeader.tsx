8155b03ace7fe9ef6a65efec56d529231db2edbe
김보미 <kimbomi2172@naver.com>
2024-08-06 20:17:56 +0900

    ✨ JE-69-feat: userInfo 정보 헤더에 반영하기

--- src/components/commons/Header/_components/LoggedInHeader.tsx
+++ src/components/commons/Header/_components/LoggedInHeader.tsx
@@ -1,26 +1,36 @@
-import tripzipFavicon from '@/../public/logo/tripzipFavicon.png';
-import { NotificationIcon } from '@/libs/utils/Icon';
+import { getUser } from '@/libs/api/user';
+import { BaseProfile, NotificationIcon } from '@/libs/utils/Icon';
+import { useQuery } from '@tanstack/react-query';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 export default function LoggedInHeader() {
+  const { data: userInfo } = useQuery({
+    queryKey: ['userInfo'],
+    queryFn: getUser,
+  });
+
   return (
     <div className="flex items-center gap-16">
       <div className="cursor-pointer" aria-label="알림">
         <NotificationIcon />
       </div>
       <Link href="/mypage" className="flex items-center gap-6 border-l-2 pl-16">
-        <div className="h-40 w-40 overflow-hidden rounded-full border">
-          <Image
-            src={tripzipFavicon}
-            alt="trip.zip"
-            className="object-cover"
-            width={40}
-            height={40}
-          />
+        <div className="flex h-40 w-40 items-center justify-center overflow-hidden rounded-full border">
+          {userInfo?.profileImageUrl ? (
+            <Image
+              src={userInfo?.profileImageUrl}
+              alt="trip.zip"
+              className="h-full w-full object-cover"
+              width={40}
+              height={40}
+            />
+          ) : (
+            <BaseProfile className="h-full w-full object-cover" />
+          )}
         </div>
-        <p className="font-semibold">김루피</p>
+        <p className="font-semibold">{userInfo?.nickname}</p>
       </Link>
     </div>
   );
