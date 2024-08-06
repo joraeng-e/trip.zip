226af84f0f5a82961da0dd8e24dfda4fd9197425
김보미 <kimbomi2172@naver.com>
2024-08-06 20:38:59 +0900

    ✨ JE-69-feat: 마이페이지, 로그아웃 드롭박스로 구현

--- src/components/commons/Header/_components/LoggedInHeader.tsx
+++ src/components/commons/Header/_components/LoggedInHeader.tsx
@@ -1,22 +1,47 @@
+import useClickOutside from '@/hooks/useClickOutside';
 import { getUser } from '@/libs/api/user';
 import { BaseProfile, NotificationIcon } from '@/libs/utils/Icon';
 import { useQuery } from '@tanstack/react-query';
+import { deleteCookie } from 'cookies-next';
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useRef, useState } from 'react';
 
 export default function LoggedInHeader() {
+  const [isProfileBoxVisible, setIsProfileBoxVisible] = useState(false);
+
   const { data: userInfo } = useQuery({
     queryKey: ['userInfo'],
     queryFn: getUser,
   });
 
+  const handleProfileClick = () => {
+    setIsProfileBoxVisible(!isProfileBoxVisible);
+  };
+
+  const router = useRouter();
+
+  const logout = () => {
+    deleteCookie('accessToken');
+    deleteCookie('refreshToken');
+    deleteCookie('isLogin');
+    router.push('/');
+  };
+
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  useClickOutside(dropdownRef, () => setIsProfileBoxVisible(false));
+
   return (
-    <div className="flex items-center gap-16">
+    <div className="relative flex items-center gap-16">
       <div className="cursor-pointer" aria-label="알림">
         <NotificationIcon />
       </div>
-      <Link href="/mypage" className="flex items-center gap-6 border-l-2 pl-16">
+      <div
+        className="flex cursor-pointer items-center gap-6 border-l-2 pl-16"
+        onClick={handleProfileClick}
+      >
         <div className="flex h-40 w-40 items-center justify-center overflow-hidden rounded-full border">
           {userInfo?.profileImageUrl ? (
             <Image
@@ -31,7 +56,37 @@ export default function LoggedInHeader() {
           )}
         </div>
         <p className="font-semibold">{userInfo?.nickname}</p>
-      </Link>
+      </div>
+      {isProfileBoxVisible && (
+        <motion.div
+          className="absolute right-0 top-50 z-10 h-fit w-fit cursor-pointer rounded-md border-2 border-custom-gray-200 bg-white p-4 shadow-md md:bottom-135 md:right-0"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.3 }}
+          ref={dropdownRef}
+        >
+          <div className="absolute -top-2 right-20 rotate-90">
+            <div className="pointer-events-none -translate-x-15 -translate-y-1/2 transform border-b-[11px] border-l-[11px] border-b-custom-gray-200 border-l-transparent" />
+            <div className="pointer-events-none -translate-x-15 -translate-y-1/2 transform border-l-[11px] border-t-[11px] border-l-transparent border-t-custom-gray-200" />
+          </div>
+          <Link href="/mypage">
+            <p
+              className="z-50 rounded-md p-4 text-center text-sm-medium transition-all hover:bg-custom-gray-300"
+              onClick={() => setIsProfileBoxVisible(false)}
+            >
+              마이페이지
+            </p>
+          </Link>
+          <hr className="my-2 border border-gray-200" />
+          <p
+            className="z-50 rounded-md p-4 text-center text-sm-medium transition-all hover:bg-custom-gray-300"
+            onClick={logout}
+          >
+            로그아웃
+          </p>
+        </motion.div>
+      )}
     </div>
   );
 }
