8eec96b4b46ef74bbf6dffe5a070fcb1ee62544f
alexgoni <goni000211@gmail.com>
2024-08-09 07:34:22 +0900

    ♻️ JE-77-refactor: style refactoring

--- src/components/activities/CategoryMenu.tsx
+++ src/components/activities/CategoryMenu.tsx
@@ -1,4 +1,5 @@
 import classNames from '@/libs/utils/classNames';
+import { motion } from 'framer-motion';
 import { useState } from 'react';
 
 const CATEGORIES = ['문화 · 예술', '식음료', '스포츠', '투어', '관광', '웰빙'];
@@ -25,22 +26,23 @@ export default function CategoryMenu({
       <div className="no-scrollbar flex w-full gap-8 overflow-x-auto md:gap-14 xl:gap-24">
         {CATEGORIES.map((category) => {
           const classnames = classNames(
-            'flex-shrink-0 rounded-[15px] border border-custom-green-200 px-10 py-8 leading-26 md:px-36 md:py-16 md:text-18 xl:px-48',
+            'flex-shrink-0 rounded-[15px] transition-colors duration-300 ease-in-out border border-custom-green-200 px-10 py-8 leading-26 md:px-36 md:py-16 md:text-18 xl:px-48',
             {
               'bg-custom-green-200 text-white': activeCategory === category,
-              'bg-white text-custom-green-200': activeCategory !== category,
+              'bg-white text-custom-green-200 hover:bg-[#125742] hover:text-white':
+                activeCategory !== category,
             },
           );
 
           return (
-            <button
+            <motion.button
               key={category}
               type="button"
               className={classnames}
               onClick={() => handleClick(category)}
             >
               {category}
-            </button>
+            </motion.button>
           );
         })}
         <div className="ml-16"></div>
