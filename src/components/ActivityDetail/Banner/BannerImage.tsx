19b40eae2a1b9a2fb6ee789e2ee797e67abb4cb2
dudwns0213 <dudwns0213@gmail.com>
2024-08-15 02:06:08 +0900

    ✨ JE-85-feat: BannerImage 1개인 경우 조건부 렌더링 추가

--- src/components/ActivityDetail/Banner/BannerImage.tsx
+++ src/components/ActivityDetail/Banner/BannerImage.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 import ImageModal from './BannerImageModal';
+import BlurBannerImage from './BlurBannerImage';
 
 interface ImageProps {
   bannerImageUrl: string;
@@ -43,104 +44,112 @@ export default function BannerImage(props: ImageProps) {
 
   return (
     <div className="hidden pt-10 md:block">
-      <div
-        className={`grid grid-cols-1 gap-2 md:grid-cols-2 ${className} relative`}
-      >
-        <div className={commonContainerClass}>
-          <Image
-            src={bannerImageUrl}
-            alt="banner"
-            fill
-            className={`rounded-l-xl ${commonImageClass}`}
-          />
+      {subImageUrl && subImageUrl.length === 0 ? (
+        <div
+          className={`relative flex h-500 w-full items-center justify-center overflow-hidden rounded-xl ${className}`}
+        >
+          <BlurBannerImage src={bannerImageUrl} alt="banner" />
         </div>
-
-        {subImageUrl && subImageUrl.length === 1 && (
-          <div className="col-span-1 grid grid-cols-1 gap-2">
-            <div className={commonContainerClass}>
-              <Image
-                src={subImageUrl[0]}
-                alt="sub-image-0"
-                fill
-                className={`rounded-r-xl ${commonImageClass}`}
-              />
-            </div>
+      ) : (
+        <div
+          className={`grid grid-cols-1 gap-2 md:grid-cols-2 ${className} relative`}
+        >
+          <div className={commonContainerClass}>
+            <Image
+              src={bannerImageUrl}
+              alt="banner"
+              fill
+              className={`rounded-l-xl ${commonImageClass}`}
+            />
           </div>
-        )}
 
-        {subImageUrl && subImageUrl.length === 2 && (
-          <div className="col-span-1 grid grid-cols-1 gap-2">
-            <div className={commonContainerClass}>
-              <Image
-                src={subImageUrl[0]}
-                alt="sub-image-0"
-                fill
-                className={`rounded-r-xl ${commonImageClass}`}
-              />
-            </div>
-          </div>
-        )}
-
-        {subImageUrl && subImageUrl.length === 3 && (
-          <div className="col-span-1 grid grid-cols-2 gap-2">
-            {subImageUrl.slice(0, 3).map((url, index) => (
-              <div key={index} className={smallImageClass}>
+          {subImageUrl && subImageUrl.length === 1 && (
+            <div className="col-span-1 grid grid-cols-1 gap-2">
+              <div className={commonContainerClass}>
                 <Image
-                  src={url}
-                  alt={`sub-image-${index}`}
+                  src={subImageUrl[0]}
+                  alt="sub-image-0"
                   fill
-                  className={commonImageClass}
+                  className={`rounded-r-xl ${commonImageClass}`}
                 />
               </div>
-            ))}
-            <div className={smallImageClass}>
-              <Image
-                src={NoImage}
-                alt="base-profile"
-                fill
-                className={`rounded-br-xl ${commonImageClass}`}
-              />
             </div>
-          </div>
-        )}
+          )}
 
-        {subImageUrl && subImageUrl.length > 3 && (
-          <div className="col-span-1 grid grid-cols-2 gap-2">
-            {subImageUrl.slice(0, 4).map((url, index) => {
-              const roundedClass =
-                index === 1
-                  ? 'rounded-tr-xl'
-                  : index === 3
-                    ? 'rounded-br-xl'
-                    : '';
-              return (
+          {subImageUrl && subImageUrl.length === 2 && (
+            <div className="col-span-1 grid grid-cols-1 gap-2">
+              <div className={commonContainerClass}>
+                <Image
+                  src={subImageUrl[0]}
+                  alt="sub-image-0"
+                  fill
+                  className={`rounded-r-xl ${commonImageClass}`}
+                />
+              </div>
+            </div>
+          )}
+
+          {subImageUrl && subImageUrl.length === 3 && (
+            <div className="col-span-1 grid grid-cols-2 gap-2">
+              {subImageUrl.slice(0, 3).map((url, index) => (
                 <div key={index} className={smallImageClass}>
                   <Image
                     src={url}
                     alt={`sub-image-${index}`}
                     fill
-                    className={`${commonImageClass} ${roundedClass}`}
+                    className={commonImageClass}
                   />
                 </div>
-              );
-            })}
-          </div>
-        )}
+              ))}
+              <div className={smallImageClass}>
+                <Image
+                  src={NoImage}
+                  alt="base-profile"
+                  fill
+                  className={`rounded-br-xl ${commonImageClass}`}
+                />
+              </div>
+            </div>
+          )}
 
-        <button
-          onClick={handleOpenModal}
-          className="absolute bottom-20 right-20 h-40 w-120 rounded-3xl border border-custom-gray-400 bg-white text-md-regular transition hover:bg-custom-gray-800 hover:text-white"
-        >
-          사진 전체 보기
-        </button>
-        <div className="max-w-1200">
-          <ImageModal
-            isOpen={isModalOpen}
-            onClose={handleCloseModal}
-            images={Images}
-          />
+          {subImageUrl && subImageUrl.length > 3 && (
+            <div className="col-span-1 grid grid-cols-2 gap-2">
+              {subImageUrl.slice(0, 4).map((url, index) => {
+                const roundedClass =
+                  index === 1
+                    ? 'rounded-tr-xl'
+                    : index === 3
+                      ? 'rounded-br-xl'
+                      : '';
+                return (
+                  <div key={index} className={smallImageClass}>
+                    <Image
+                      src={url}
+                      alt={`sub-image-${index}`}
+                      fill
+                      className={`${commonImageClass} ${roundedClass}`}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          )}
+
+          <button
+            onClick={handleOpenModal}
+            className="absolute bottom-20 right-20 z-50 h-40 w-120 rounded-3xl border border-custom-gray-400 bg-white text-md-regular transition hover:bg-custom-gray-800 hover:text-white"
+          >
+            사진 전체 보기
+          </button>
+          <div className="max-w-1200">
+            <ImageModal
+              isOpen={isModalOpen}
+              onClose={handleCloseModal}
+              images={Images}
+            />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
