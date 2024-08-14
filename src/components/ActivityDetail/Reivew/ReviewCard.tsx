e0119029f7b32aff1d5776b20cb3ff09b46e8d7b
dudwns0213 <dudwns0213@gmail.com>
2024-08-14 11:39:49 +0900

    ✨ JE-85-feat: 프로필 이미지 없을 시 기본이미지 png 수정

--- src/components/ActivityDetail/Reivew/ReviewCard.tsx
+++ src/components/ActivityDetail/Reivew/ReviewCard.tsx
@@ -1,5 +1,6 @@
+import BaseImage from '@/../public/imgs/baseProfile.png';
 import useExtractTags from '@/hooks/useExtractTags';
-import { BaseProfile } from '@/libs/utils/Icon';
+import { ReviewResponse } from '@trip.zip-api';
 import Image from 'next/image';
 import React from 'react';
 
@@ -8,49 +9,52 @@ import ExpandableText from '../ExpandableText';
 import StarRating from './StarRating';
 
 interface ReviewCardProps {
-  user: {
-    profileImageUrl: string | null;
-    nickname: string;
-    id: number;
-  };
-  rating: number;
-  content: string;
-  createdAt: string;
-  maxLength?: number;
+  data: ReviewResponse;
 }
 
 export default function ReviewCard(props: ReviewCardProps) {
-  const { user, rating, content, createdAt, maxLength = 100 } = props;
+  const { data } = props;
 
-  const { tags: extractedTags, textWithoutTags } = useExtractTags(content);
+  const { tags: extractedTags, textWithoutTags } = useExtractTags(data.content);
 
   return (
     <div className="contour">
-      <div className="pt-12">
-        <div className="flex items-center gap-12">
-          <div className="relative h-40 w-40 overflow-hidden rounded-full">
-            <Image
-              src={user.profileImageUrl || BaseProfile}
-              alt={user.nickname}
-              fill
-            />
-          </div>
-          <div>
-            <div className="text-lg-bold text-nomad-black">{user.nickname}</div>
-            <div className="text-lg-regular text-custom-gray-500">
-              {new Date(createdAt).toLocaleDateString()}
+      <div>
+        <div className="pt-12">
+          <div className="flex items-center gap-12">
+            <div className="relative h-40 w-40 overflow-hidden rounded-full">
+              <Image
+                src={
+                  data.user.profileImageUrl
+                    ? data.user.profileImageUrl
+                    : BaseImage
+                }
+                alt={data.user.nickname}
+                fill
+                className="object-cover"
+              />
+            </div>
+            {}
+            <div>
+              <div className="text-lg-bold text-nomad-black">
+                {data.user.nickname}
+              </div>
+              <div className="text-lg-regular text-custom-gray-500">
+                {new Date(data.createdAt).toLocaleDateString()}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="relative my-10 flex items-center gap-12">
-          <StarRating rating={rating} />
-          <div className="pt-2 text-lg-regular text-custom-gray-700">
-            {rating}
+          <div className="relative my-10 flex items-center gap-12">
+            <StarRating rating={data.rating} />
+            <div className="pt-2 text-lg-regular text-custom-gray-700">
+              {data.rating}
+            </div>
           </div>
-        </div>
-        <ActivityTags extractedTags={extractedTags} />
-        <div className="mt-2 text-lg-regular text-nomad-black">
-          <ExpandableText text={textWithoutTags} maxLength={maxLength} />
+
+          <div className="mt-2 text-lg-regular text-nomad-black">
+            <ExpandableText text={textWithoutTags} />
+          </div>
+          <ActivityTags extractedTags={extractedTags} />
         </div>
       </div>
     </div>
