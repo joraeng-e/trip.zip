c731970707bb156d6a763d582fcea7201b8982c4
김보미 <kimbomi2172@naver.com>
2024-08-14 16:52:19 +0900

    ♻️ JE-88-refactor: 소셜 auth 로딩 구현

--- src/pages/login/oauth/kakao.tsx
+++ src/pages/login/oauth/kakao.tsx
@@ -1,3 +1,4 @@
+import Loading from '@/components/commons/Loading';
 import { notify } from '@/components/commons/Toast';
 import { signInUser } from '@/libs/api/oauth';
 import { SignInRequest, SignInResponse } from '@trip.zip-api';
@@ -54,5 +55,5 @@ export default function Kakao() {
     handleOAuthCallback();
   }, [router.query]);
 
-  return <div>{loading ? <p>로딩 중...</p> : <p>카카오 로그인 완료!</p>}</div>;
+  return <div>{loading && <Loading />}</div>;
 }
