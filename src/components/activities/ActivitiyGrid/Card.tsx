1ff6d1fb6103e18ba66c1c2694314edd7c7083b0
김보미 <kimbomi2172@naver.com>
2024-08-16 15:19:00 +0900

    💄 JE-92-ui: activities 페이지 다크모드 적용

--- src/components/activities/ActivitiyGrid/Card.tsx
+++ src/components/activities/ActivitiyGrid/Card.tsx
@@ -34,7 +34,7 @@ function ActivityCard({ data }: { data: Activity }) {
           {rating} <span className="text-[#a1a1a1]">({reviewCount})</span>
         </span>
       </div>
-      <h1 className="line-clamp-2 text-18 font-semibold text-nomad-black md:text-24">
+      <h1 className="line-clamp-2 text-18 font-semibold text-nomad-black md:text-24 dark:text-white">
         {title}
       </h1>
       <div className="mb-4 mt-12">
@@ -53,16 +53,16 @@ export function ActivityCardSkeleton() {
   return (
     <div className="w-full">
       <div className="relative aspect-square overflow-hidden rounded-[20px] bg-slate-300">
-        <div className="animate-shimmer absolute inset-0 bg-gradient-to-r from-transparent via-gray-100 to-transparent opacity-50" />
+        <div className="absolute inset-0 animate-shimmer bg-gradient-to-r from-transparent via-gray-100 to-transparent opacity-50" />
       </div>
       <div className="relative my-4 h-18 w-1/3 overflow-hidden rounded bg-slate-300 md:my-8 md:h-22 md:w-1/6">
-        <div className="animate-shimmer absolute inset-0 bg-gradient-to-r from-transparent via-gray-100 to-transparent opacity-50" />
+        <div className="absolute inset-0 animate-shimmer bg-gradient-to-r from-transparent via-gray-100 to-transparent opacity-50" />
       </div>
       <div className="relative h-28 w-3/4 overflow-hidden rounded bg-slate-300 md:h-32 md:w-2/3">
-        <div className="animate-shimmer absolute inset-0 bg-gradient-to-r from-transparent via-gray-100 to-transparent opacity-50" />
+        <div className="absolute inset-0 animate-shimmer bg-gradient-to-r from-transparent via-gray-100 to-transparent opacity-50" />
       </div>
       <div className="relative mb-4 mt-12 h-28 w-1/2 overflow-hidden rounded bg-slate-300 md:w-1/3">
-        <div className="animate-shimmer absolute inset-0 bg-gradient-to-r from-transparent via-gray-100 to-transparent opacity-50" />
+        <div className="absolute inset-0 animate-shimmer bg-gradient-to-r from-transparent via-gray-100 to-transparent opacity-50" />
       </div>
     </div>
   );
