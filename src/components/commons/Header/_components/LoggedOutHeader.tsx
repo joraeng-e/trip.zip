03f47c653c031028a9991c0403e47a390969f6f0
김보미 <kimbomi2172@naver.com>
2024-08-19 15:24:02 +0900

    💄 JE-100-ui: 다크모드 UI 추가

--- src/components/commons/Header/_components/LoggedOutHeader.tsx
+++ src/components/commons/Header/_components/LoggedOutHeader.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 
 const linkTextStyle =
-  'transition-all duration-500 hover:text-custom-green-200 hover:underline hover:decoration-custom-green-200 hover:decoration-2 hover:underline-offset-4 hover:transition-all';
+  'transition-all duration-500 hover:text-custom-green-200 hover:underline hover:decoration-custom-green-200 hover:decoration-2 hover:underline-offset-4 hover:transition-all dark:hover:text-custom-green-100 dark:hover:decoration-custom-green-100';
 
 export default function LoggedOutHeader() {
   return (
