2139e08ba83649666216cb50681c91a29c1f29a6
김보미 <kimbomi2172@naver.com>
2024-08-19 13:53:11 +0900

    ♻️ JE-102-refactor: 카드 Link 추가

--- src/components/mypage/ReservationList/ReservationCard.tsx
+++ src/components/mypage/ReservationList/ReservationCard.tsx
@@ -5,7 +5,8 @@ import { patchMyReservationStatus } from '@/libs/api/myReservations';
 import { useMutation } from '@tanstack/react-query';
 import { Reservation } from '@trip.zip-api';
 import Image from 'next/image';
-import React, { useEffect } from 'react';
+import Link from 'next/link';
+import React from 'react';
 
 type ReservationCardProps = {
   reservation: Reservation;
@@ -103,19 +104,27 @@ export default function ReservationCard({
 
   return (
     <div className="dark-border mb-16 flex h-153 max-w-800 gap-20 overflow-hidden rounded-xl shadow-md lg:h-204">
-      <div className="relative h-full w-128 flex-shrink-0 md:w-156 lg:w-204">
+      <Link
+        href={`/activity/${id}`}
+        className="relative h-full w-128 flex-shrink-0 md:w-156 lg:w-204"
+      >
         <Image
           src={bannerImageUrl}
           alt={title}
           layout="fill"
           objectFit="cover"
         />
-      </div>
+      </Link>
       <div className="flex w-full flex-col justify-between py-10">
         <p className="mb-2 text-gray-600">{statusValue(status)}</p>
-        <h2 className="text-xl mb-2 text-lg-semibold">{title}</h2>
+        <Link
+          href={`/activity/${id}`}
+          className="text-2lg-bold lg:text-xl-bold"
+        >
+          {title}
+        </Link>
         <p className="text-gray-600">
-          {date} {startTime} - {endTime} {headCount}명
+          {date} {startTime} - {endTime} ({headCount}명)
         </p>
         <div className="flex items-baseline justify-between pb-10 pr-10 md:pr-20">
           <p className="mb-2 text-md-semibold md:text-xl-semibold">
