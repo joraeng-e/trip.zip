4695a63c58eddf5c3764edb92f49b819f60500b9
alexgoni <goni000211@gmail.com>
2024-08-20 16:26:17 +0900

    🐛 JE-106-fix: 빌드 에러 수정

--- next.config.js
+++ next.config.js
@@ -8,25 +8,26 @@ const withPWA = require('next-pwa')({
 });
 
 /** @type {import('next').NextConfig} */
-module.exports = withPWA(
-  withBundleAnalyzer({
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
+const nextConfig = {
+  transpilePackages: ['@uiw/react-md-editor', '@uiw/react-markdown-preview'],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
 
-      return config;
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
-          port: '',
-          pathname: '/globalnomad/**',
-        },
-      ],
-    },
-  }),
-);
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/globalnomad/**',
+      },
+    ],
+  },
+};
+
+module.exports = withPWA(withBundleAnalyzer(nextConfig));
