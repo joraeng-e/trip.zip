9924ca808f47d4190f7c812fa0bcace087d0d91d
alexgoni <goni000211@gmail.com>
2024-08-20 16:13:46 +0900

    📦 JE-106-chore: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,26 +1,32 @@
-/** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
 });
 
-module.exports = withPWA({
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+/** @type {import('next').NextConfig} */
+module.exports = withPWA(
+  withBundleAnalyzer({
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
 
-    return config;
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
-        port: '',
-        pathname: '/globalnomad/**',
-      },
-    ],
-  },
-});
+      return config;
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+          port: '',
+          pathname: '/globalnomad/**',
+        },
+      ],
+    },
+  }),
+);
