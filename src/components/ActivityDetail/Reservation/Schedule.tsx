d0fdf6eec1dfd2cecaa145b78e558b3f6b452d9b
김보미 <kimbomi2172@naver.com>
2024-08-21 17:10:16 +0900

    💄 JE-113-ui: border 설정

--- src/components/ActivityDetail/Reservation/Schedule.tsx
+++ src/components/ActivityDetail/Reservation/Schedule.tsx
@@ -190,10 +190,15 @@ export default function Schedule(props: ScheduleProps) {
         <div className="w-800">
           <h2 className="ml-10 text-2xl-bold">예약 확인</h2>
 
-          <div className="mt-4 rounded-lg bg-white p-4 shadow-lg">
+          <div className="mt-4 rounded-lg border-2 border-custom-gray-400 bg-white">
             <div className="flex">
               <div className="relative mr-4 h-200 w-200">
-                <Image src={bannerImageUrl} alt={title} fill />
+                <Image
+                  src={bannerImageUrl}
+                  alt={title}
+                  fill
+                  className="rounded-l-lg"
+                />
               </div>
 
               <div className="relative mx-20 my-10 flex-1">
@@ -213,7 +218,7 @@ export default function Schedule(props: ScheduleProps) {
           </div>
 
           <div className="">
-            <div className="my-20 rounded-lg py-20 shadow-lg">
+            <div className="my-20 rounded-lg border-2 border-custom-gray-400 py-20">
               <h4 className="ml-20 text-xl-bold">예약자 정보</h4>
               <div className="my-10 ml-20">
                 <div className="mb-4 text-lg-medium">
@@ -223,7 +228,7 @@ export default function Schedule(props: ScheduleProps) {
               </div>
             </div>
 
-            <div className="my-20 rounded-lg py-20 shadow-lg">
+            <div className="my-20 rounded-lg border-2 border-custom-gray-400 py-20">
               <h4 className="ml-20 text-xl-bold">예약 내용</h4>
               <div className="mt-2 flex justify-between">
                 <div className="my-10 ml-20">
