5c38c8eef1c8d460088445929fac39bb5113184a
dudwns0213 <dudwns0213@gmail.com>
2024-08-22 02:41:07 +0900

    ✨ JE-114-feat: 모바일 로그인, 체험 수정, 예약 버튼 조건부 렌더링

--- src/components/ActivityDetail/Reservation/MobileReservationFooter.tsx
+++ src/components/ActivityDetail/Reservation/MobileReservationFooter.tsx
@@ -1,4 +1,6 @@
 import { GetActivityDetailResponse } from '@trip.zip-api';
+import { getCookie } from 'cookies-next';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 import Button from '../../commons/Button';
@@ -14,6 +16,7 @@ export default function MobileReservationFooter(
 ) {
   const { data, isSameUser } = props;
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -23,17 +26,49 @@ export default function MobileReservationFooter(
     setIsModalOpen(false);
   };
 
+  const handleLogin = () => {
+    router.push('/login');
+  };
+
+  const handleEditExperience = () => {
+    if (isSameUser) {
+      router.push('/mypage/myActivities');
+    }
+  };
+
   return (
     <>
       <div className="md:hidden">
         <div className="fixed bottom-0 left-0 right-0 h-70 border-t-2 bg-white p-10 shadow-lg">
-          <Button
-            variant="activeButton"
-            className="h-36 rounded-md text-md-bold"
-            onClick={handleOpenModal}
-          >
-            예약하기
-          </Button>
+          {!getCookie('refreshToken') && (
+            <Button
+              variant="activeButton"
+              className="mb-2 h-36 rounded-md text-md-bold"
+              onClick={handleLogin}
+            >
+              로그인하기
+            </Button>
+          )}
+
+          {isSameUser && (
+            <Button
+              variant="activeButton"
+              className="h-36 rounded-md text-md-bold"
+              onClick={handleEditExperience}
+            >
+              체험 수정하기
+            </Button>
+          )}
+
+          {getCookie('refreshToken') && !isSameUser && (
+            <Button
+              variant="activeButton"
+              className="mb-2 h-36 rounded-md text-md-bold"
+              onClick={handleOpenModal}
+            >
+              예약하기
+            </Button>
+          )}
         </div>
         <MobileReservationModal
           isOpen={isModalOpen}
