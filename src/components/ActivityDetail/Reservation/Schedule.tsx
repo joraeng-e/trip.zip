2743b6f9a79d7e89fdabcbd2e998e623e52e743b
dudwns0213 <dudwns0213@gmail.com>
2024-08-22 02:33:46 +0900

    ♻️ JE-114-refactor: 예약신청 컴파운드 패턴 모달 적용

--- src/components/ActivityDetail/Reservation/Schedule.tsx
+++ src/components/ActivityDetail/Reservation/Schedule.tsx
@@ -1,4 +1,5 @@
 import Button from '@/components/commons/Button';
+import Modal from '@/components/commons/Modal';
 import { notify } from '@/components/commons/Toast';
 import { postReservations } from '@/libs/api/activities';
 import { getUser } from '@/libs/api/user';
@@ -14,8 +15,6 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { FaStar } from 'react-icons/fa';
-
-import BaseModal from '../BaseModal';
 
 interface ScheduleProps {
   selectedSchedules: { startTime: string; endTime: string; id: number }[];
@@ -51,7 +50,6 @@ export default function Schedule(props: ScheduleProps) {
   const { activityid } = router.query;
   const activityId = Number(activityid);
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedScheduleId, setSelectedScheduleId] = useState<number | null>(
     null,
   );
@@ -77,8 +75,6 @@ export default function Schedule(props: ScheduleProps) {
       });
     } else if (isSameUser) {
       router.push('/mypage/myActivities');
-    } else {
-      setIsModalOpen(true);
     }
   };
 
@@ -87,7 +83,6 @@ export default function Schedule(props: ScheduleProps) {
   const mutation = useMutation({
     mutationFn: postReservations,
     onSuccess: () => {
-      setIsModalOpen(false);
       if (onReservationComplete) {
         onReservationComplete();
         queryClient.invalidateQueries({ queryKey: ['reservations'] });
@@ -95,7 +90,6 @@ export default function Schedule(props: ScheduleProps) {
       notify('success', '예약이 성공적으로 완료되었습니다.');
     },
     onError: (error: Error) => {
-      setIsModalOpen(false);
       if (onReservationComplete) {
         onReservationComplete();
       }
@@ -160,106 +154,110 @@ export default function Schedule(props: ScheduleProps) {
 
           <Button
             variant="activeButton"
-            className="mt-4 h-36 rounded-md text-md-bold"
-            onClick={() => {
-              if (isSameUser) {
-                handleReservationClick();
-              } else {
-                if (selectedScheduleId === null) {
-                  notify('warning', '스케줄을 선택해 주세요.');
-                } else {
-                  handleReservationClick();
-                }
-              }
-            }}
+            className={`mt-4 h-36 rounded-md text-md-bold ${!isSameUser ? 'hidden' : ''}`}
+            onClick={handleReservationClick}
           >
-            {isSameUser ? '체험 수정하기' : '예약하기'}
+            체험 수정하기
           </Button>
         </>
       )}
-
-      <BaseModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        className="dark-base mx-40 h-auto p-20 text-nomad-black"
-      >
-        <div className="w-800">
-          <h2 className="ml-10 text-2xl-bold">예약 확인</h2>
-
-          <div className="dark-base dark-border mt-20 rounded-lg bg-white shadow-lg">
-            <div className="flex">
-              <div className="relative mr-4 h-200 w-200">
-                <Image
-                  src={bannerImageUrl}
-                  alt={title}
-                  fill
-                  className="rounded-l-md"
-                />
-              </div>
-
-              <div className="relative mx-20 my-14 flex-1 p-4">
-                <h3 className="text-xl-bold">{title}</h3>
-                <p className="mt-6 text-md-semibold text-custom-gray-700">
-                  {address}
-                </p>
-                <div className="mt-10 flex items-center">
-                  <FaStar className="mb-0 text-yellow-500" />
-                  <span className="ml-4 text-md-regular">{rating}</span>
-                </div>
-                <p className="absolute bottom-0 right-0 text-2xl-semibold">
-                  {price.toLocaleString()} 원
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="">
-            <div className="dark-border my-20 rounded-lg py-20 shadow-lg">
-              <h4 className="ml-20 text-xl-bold">예약자 정보</h4>
-              <div className="my-10 ml-20">
-                <div className="mb-4 text-lg-medium">
-                  예약자: {userData?.nickname}
-                </div>
-                <div className="text-lg-medium">이메일: {userData?.email}</div>
-              </div>
-            </div>
-
-            <div className="dark-border my-20 rounded-lg py-20 shadow-lg">
-              <h4 className="ml-20 text-xl-bold">예약 내용</h4>
-              <div className="mt-2 flex justify-between">
-                <div className="my-10 ml-20">
-                  <div className="mb-4 text-lg-medium">
-                    인원 수: {guestCount}
-                  </div>
-                  <div className="text-lg-medium">
-                    예약일시: {moment(selectedDate).format('YYYY년 MM월 DD일')}{' '}
-                    /{' '}
-                    {activeIndex !== null && selectedSchedules[activeIndex]
-                      ? `${selectedSchedules[activeIndex].startTime} ~ ${selectedSchedules[activeIndex].endTime}`
-                      : '선택된 일정이 없습니다.'}
-                  </div>
-                </div>
-              </div>
-              <hr className="contour" />
-              <div className="mx-20 mt-10 flex justify-between text-xl-bold">
-                총 상품 금액
-                <div className="text-custom-green-300">
-                  {(guestCount * price).toLocaleString()} 원
-                </div>
-              </div>
-            </div>
-          </div>
-
+      <Modal.Root>
+        <Modal.Trigger disabled={selectedScheduleId === null}>
           <Button
             variant="activeButton"
-            className="mt-4 rounded-md"
-            onClick={handleSubmitReservation}
-            hasICon
+            className={`mt-4 h-36 rounded-md text-md-bold ${isSameUser ? 'hidden' : ''}`}
+            onClick={() => {
+              if (selectedScheduleId === null) {
+                notify('warning', '스케줄을 선택해 주세요.');
+              } else {
+                handleReservationClick();
+              }
+            }}
           >
             예약하기
           </Button>
-        </div>
-      </BaseModal>
+        </Modal.Trigger>
+
+        <Modal.Content>
+          <Modal.Description>
+            <div className="h-auto w-800">
+              <h2 className="ml-10 text-2xl-bold">예약 확인</h2>
+
+              <div className="dark-base dark-border mt-20 rounded-lg bg-white shadow-lg">
+                <div className="flex">
+                  <div className="relative mr-4 h-200 w-200">
+                    <Image
+                      src={bannerImageUrl}
+                      alt={title}
+                      fill
+                      className="rounded-l-md"
+                    />
+                  </div>
+
+                  <div className="relative mx-20 my-14 flex-1 p-4">
+                    <h3 className="text-xl-bold">{title}</h3>
+                    <p className="mt-6 text-md-semibold text-custom-gray-700">
+                      {address}
+                    </p>
+                    <div className="mt-10 flex items-center">
+                      <FaStar className="mb-0 text-yellow-500" />
+                      <span className="ml-4 text-md-regular">{rating}</span>
+                    </div>
+                    <p className="absolute bottom-0 right-0 text-2xl-semibold">
+                      {price.toLocaleString()} 원
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="">
+                <div className="dark-border my-20 rounded-lg py-20 shadow-lg">
+                  <h4 className="ml-20 text-xl-bold">예약자 정보</h4>
+                  <div className="my-10 ml-20">
+                    <div className="mb-4 text-lg-medium">
+                      예약자: {userData?.nickname}
+                    </div>
+                    <div className="text-lg-medium">
+                      이메일: {userData?.email}
+                    </div>
+                  </div>
+                </div>
+
+                <div className="dark-border my-20 rounded-lg py-20 shadow-lg">
+                  <h4 className="ml-20 text-xl-bold">예약 내용</h4>
+                  <div className="mt-2 flex justify-between">
+                    <div className="my-10 ml-20">
+                      <div className="mb-4 text-lg-medium">
+                        인원 수: {guestCount}
+                      </div>
+                      <div className="text-lg-medium">
+                        예약일시:{' '}
+                        {moment(selectedDate).format('YYYY년 MM월 DD일')} /{' '}
+                        {activeIndex !== null && selectedSchedules[activeIndex]
+                          ? `${selectedSchedules[activeIndex].startTime} ~ ${selectedSchedules[activeIndex].endTime}`
+                          : '선택된 일정이 없습니다.'}
+                      </div>
+                    </div>
+                  </div>
+                  <hr className="contour" />
+                  <div className="mx-20 mt-10 flex justify-between text-xl-bold">
+                    총 상품 금액
+                    <div className="text-custom-green-300">
+                      {(guestCount * price).toLocaleString()} 원
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Modal.Description>
+          <Modal.Close
+            onConfirm={handleSubmitReservation}
+            className="mt-4 rounded-md"
+          >
+            예약하기
+          </Modal.Close>
+        </Modal.Content>
+      </Modal.Root>
     </>
   );
 }
