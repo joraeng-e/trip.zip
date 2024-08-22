84f535ab6368032cbd74c21dd18f814782dbac3f
dudwns0213 <dudwns0213@gmail.com>
2024-08-23 00:59:33 +0900

    ♻️ JE-118-je-116-refactor: ImageGallery 컴포넌트 생성(Modal 컴파운드 패턴 적용)

--- src/components/ActivityDetail/Banner/BannerButton.tsx
+++ src/components/ActivityDetail/Banner/BannerButton.tsx
@@ -1,14 +1,6 @@
-interface BannerButtonProps {
-  onClick: () => void;
-}
-
-export default function BannerButton(props: BannerButtonProps) {
-  const { onClick } = props;
+export default function BannerButton() {
   return (
-    <button
-      onClick={onClick}
-      className="dark-base dark-border absolute bottom-20 right-20 h-40 w-120 rounded-3xl border border-custom-gray-400 bg-white text-md-regular transition hover:bg-custom-gray-800 hover:text-white"
-    >
+    <button className="dark-base dark-border absolute bottom-20 right-20 h-40 w-120 rounded-3xl border border-custom-gray-400 bg-white text-md-regular transition hover:bg-custom-gray-800 hover:text-white">
       사진 전체 보기
     </button>
   );
